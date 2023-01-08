b4b25531717e1a24155145232cd39a51598897d3
Hugo Hirsch <github@hugo-hirsch.de>
2023-01-08 13:52:02 +0100

    Update junit and reformat

--- src/main/java/de/aikiit/App.java
+++ src/main/java/de/aikiit/App.java
@@ -1,13 +1,7 @@
 package de.aikiit;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
