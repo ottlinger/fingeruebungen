f5c90e2cdc0f1d52e0d2aec7aadb6589cbd7ca85
Hugo Hirsch <github@hugo-hirsch.de>
2023-01-08 13:59:18 +0100

    Use newer junit

--- src/test/java/de/aikiit/AppTest.java
+++ src/test/java/de/aikiit/AppTest.java
@@ -1,9 +1,12 @@
 package de.aikiit;
 
+import org.junit.Test;
+
 import static org.junit.Assert.assertNotNull;
 
 public class AppTest {
 
+    @Test
     public void testApp() {
         assertNotNull(new App());
     }
