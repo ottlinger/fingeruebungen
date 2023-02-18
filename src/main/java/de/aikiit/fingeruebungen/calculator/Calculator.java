1c7c887c067e40c4f7a32c5d5b557f0a7ff92385
Hugo Hirsch <github@hugo-hirsch.de>
2023-02-18 17:23:12 +0100

    Ease UX

--- src/main/java/de/aikiit/fingeruebungen/calculator/Calculator.java
+++ src/main/java/de/aikiit/fingeruebungen/calculator/Calculator.java
@@ -5,18 +5,19 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) throws IOException {
-        System.out.println("Willkommen bei unserem Taschenrechner :)");
-        String line = "";
+        System.out.println("Willkommen bei unserem Taschenrechner :) (e = Ende)");
         Scanner s = new Scanner(System.in);
-        while ((line = s.nextLine()) != null) {
+        while (true) {
             System.out.println("> Erste Zahl = ");
             String ersteZahl = s.nextLine();
-            if (ersteZahl.equals(("e"))) {
-                break;
-            }
 
             System.out.println("> Zweite Zahl = ");
             String zweiteZahl = s.nextLine();
+
+            if (ersteZahl.equals("e") || zweiteZahl.equals("e")) {
+                System.out.println("Tschüßikowski");
+                break;
+            }
 
             System.out.println("==========================================");
             System.out.println("> " + ersteZahl + " + " + zweiteZahl + " = " + new Addierer(ersteZahl, zweiteZahl).getSumme());
