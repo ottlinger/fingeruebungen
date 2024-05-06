070917fe32497da30961129fc68041aba8006fa7
Hugo Hirsch <github@hugo-hirsch.de>
2024-05-06 18:10:27 +0200

    Find 68

--- src/main/java/de/aikiit/fingeruebungen/date/DateCalculator.java
+++ src/main/java/de/aikiit/fingeruebungen/date/DateCalculator.java
@@ -1,5 +1,7 @@
 package de.aikiit.fingeruebungen.date;
 
+import java.time.LocalDate;
+
 public class DateCalculator {
 
     public static void main(String[] args) {
@@ -7,7 +9,17 @@ public class DateCalculator {
     }
 
     private static void calculateDates() {
-        
+        LocalDate start = LocalDate.of(2024, 1, 1);
+        while (true) {
+            if (68 == calcSum()) {
+                System.out.println(start);
+            }
+            start.plusDays(1L);
+        }
+    }
+
+    private static int calcSum() {
+        return 0;
     }
 
 }
