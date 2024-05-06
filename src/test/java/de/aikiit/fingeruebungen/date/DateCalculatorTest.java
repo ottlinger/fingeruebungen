dba890016510ea281200112210d66de9e9167129
Hugo Hirsch <github@hugo-hirsch.de>
2024-05-06 21:53:24 +0200

    Fix test setip

--- src/test/java/de/aikiit/fingeruebungen/date/DateCalculatorTest.java
+++ src/test/java/de/aikiit/fingeruebungen/date/DateCalculatorTest.java
@@ -4,12 +4,17 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.time.LocalDate;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 public class DateCalculatorTest {
 
     @Test
     void checkNullSafety() {
+        assertThat(DateCalculator.calcSum(null)).isEqualTo(56);
+    }
+
+    @Test
+    void calculateCorrectValue() {
         assertThat(DateCalculator.calcSum(LocalDate.of(1900, 3, 17))).isEqualTo(56);
     }
 
