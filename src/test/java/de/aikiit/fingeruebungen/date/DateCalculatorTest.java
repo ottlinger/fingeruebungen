0743734034a0f34230a09b720910771582eb322c
Hugo Hirsch <github@hugo-hirsch.de>
2024-05-06 21:46:26 +0200

    Use assertJ

--- src/test/java/de/aikiit/fingeruebungen/date/DateCalculatorTest.java
+++ src/test/java/de/aikiit/fingeruebungen/date/DateCalculatorTest.java
@@ -1,5 +1,16 @@
 package de.aikiit.fingeruebungen.date;
 
+import static org.assertj.core.api.Assertions.*;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+
 public class DateCalculatorTest {
+
+    @Test
+    void checkNullSafety() {
+        assertThat(DateCalculator.calcSum(LocalDate.of(1900, 3, 17))).isEqualTo(56);
+    }
 
 }
