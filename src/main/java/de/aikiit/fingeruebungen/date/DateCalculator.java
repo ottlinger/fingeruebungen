ced27b90611625769ff5ee96114c8e4086211395
Hugo Hirsch <github@hugo-hirsch.de>
2024-05-06 21:36:28 +0200

    Add more logic

--- src/main/java/de/aikiit/fingeruebungen/date/DateCalculator.java
+++ src/main/java/de/aikiit/fingeruebungen/date/DateCalculator.java
@@ -4,22 +4,41 @@ import java.time.LocalDate;
 
 public class DateCalculator {
 
+private static final Integer TARGET = Integer.valueOf(68);
+
     public static void main(String[] args) {
+        System.out.println("Starting to calculate ....");
         DateCalculator.calculateDates();
     }
 
     private static void calculateDates() {
-        LocalDate start = LocalDate.of(2024, 1, 1);
+        LocalDate start = LocalDate.of(2022, 1, 1);
         while (true) {
-            if (68 == calcSum()) {
+            // System.out.println(start + " /// " + calcSum(start));
+            if (TARGET.equals(calcSum(start))) {
                 System.out.println(start);
             }
-            start.plusDays(1L);
+            start = start.plusDays(1L);
         }
     }
 
-    private static int calcSum() {
-        return 0;
+    private static int calcSum(LocalDate date) {
+        if (date == null) {
+            return 0;
+        }
+
+        return date.getDayOfMonth() + date.getMonthValue() + splitYear(date);
+
+    }
+
+    private static Integer splitYear(LocalDate date) {
+        if (date == null) {
+            return 0;
+        }
+
+        String year = String.valueOf(date.getYear());
+        return Integer.valueOf(year.substring(0, 1)) + Integer.valueOf(year.substring(0, 1));
+
     }
 
 }
