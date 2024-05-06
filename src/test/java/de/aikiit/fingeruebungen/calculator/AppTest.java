bc1b4ac0367486756586fa12c0b9086a135efed0
Hugo Hirsch <github@hugo-hirsch.de>
2024-05-06 21:37:38 +0200

    Package struc in tests

---
+++ src/test/java/de/aikiit/fingeruebungen/calculator/AppTest.java
@@ -0,0 +1,13 @@
+package de.aikiit;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        assertNotNull(new App());
+    }
+}
