01c7053dab70e6032da3e6dd4faf5ff7efce564a
Hugo Hirsch <github@hugo-hirsch.de>
2024-05-06 17:46:21 +0200

    Add rump

---
+++ src/main/java/de/aikiit/fingeruebungen/date/DateCalculator.java
@@ -0,0 +1,13 @@
+package de.aikiit.fingeruebungen.date;
+
+public class DateCalculator {
+
+    public static void main(String[] args) {
+        DateCalculator.calculateDates();
+    }
+
+    private static void calculateDates() {
+        
+    }
+
+}
